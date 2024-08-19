7e832e5a2c51fdf336d13a12f362382409f38cbd
Santiago García <santi.garcia3@hotmail.com>
2024-08-19 20:20:51 -0300

    TA6 resumida

--- TA6/script.js
+++ TA6/script.js
@@ -1,15 +1,14 @@
 function convertToCelsius() {
     const grados = parseInt(document.getElementById('temperatura').value);
-    const resultado = String(grados * (9/5) + 32);
+    const resultado = (grados * (9/5) + 32).toFixed(1);
     document.getElementById('resultado').innerHTML = resultado;
 }
 
 function convertToFahrenheit(){
     const grados = parseInt(document.getElementById('temperatura').value);
-    const resultado = String((grados - 32) * (5/9));
+    const resultado = ((grados - 32) * (5/9)).toFixed(1);
     document.getElementById('resultado').innerHTML = resultado;
 }
-
 
 document.getElementById('botonCelsius').addEventListener('click',convertToCelsius);
 document.getElementById('botonFar').addEventListener('click',convertToFahrenheit);
